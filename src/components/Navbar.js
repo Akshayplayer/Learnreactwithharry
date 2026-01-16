2f03d77a20f99105276efa10dba250e256a4b84a
Akshayplayer <akshayku.bothra@gmail.com>
2026-01-16 22:21:22 +0530

    tillAbout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,8 +33,8 @@ export default function Navbar(props) {
       </li> */}
     </ul>
     <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-      <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button> 
+      {/* <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+      <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>  */}
       {/*primary is used for blue color, success is used for green color, danger is used for red color */}
     </form>
   </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,6 +3,7 @@ import React,{ useState} from 'react'
 export default function TextForm(props) {
   const[text,setText]=useState(''); //here text is a variable which is actually state of the component and setText is a function for updating the text variable.
   const [textColor, setTextColor] = useState('black')
+  const[btnColor, setBtnColor] = useState('Color Red')
   const handleUpClick=()=>{
     console.log("Uppercase was clicked"+text);
     let newText=text.toUpperCase();
@@ -15,8 +16,20 @@ export default function TextForm(props) {
   }
 
   const handleRedClick=()=>{
-    console.log("Lowercase was clicked"+text);
-    setTextColor('red');
+    if(textColor === 'black'){
+      setTextColor('red');
+      setBtnColor('Color Black');
+    }
+    else{
+      setTextColor('black');
+      setBtnColor('Color Red');
+    }
+  }
+
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
   }
 
   const handleOnChange=(event)=>{
@@ -30,19 +43,20 @@ export default function TextForm(props) {
     <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea color={textColor} className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea style={{color: textColor}} className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
         <div className="btn btn-primary mx-2" onClick={handleUpClick}>Convert To UpperCase</div>
         <div className="btn btn-primary mx-2" onClick={handleLoClick}>Convert To LowerCase</div>
-        <div className="btn btn-primary mx-2" onClick={handleRedClick}>ColorRed</div>
-
+        <div className="btn btn-primary mx-2" onClick={handleRedClick}>{btnColor}</div>
+        <div className="btn btn-primary mx-2" onClick={handleCopy}>CopyText</div>
     </div>
+
     <div className="container my-2">
       <h1>Your text summary</h1>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p> Time to Read: {0.008 * text.split(" ").length} Minutes read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p style={{color: textColor}} >{text}</p>
     </div>
     </>
   )
